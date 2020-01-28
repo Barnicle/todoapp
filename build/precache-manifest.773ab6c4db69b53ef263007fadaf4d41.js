e2189f0bca8bca85126e40c88eb4859c94e98108
Barnicle <egalimov95@gmail.com>
2020-01-29 01:46:52 +0300

    build

---
+++ build/precache-manifest.773ab6c4db69b53ef263007fadaf4d41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae965daff838e49852380122719a5a07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0feae1450065efdcfdf6",
+    "url": "/static/css/main.d5af4b27.chunk.css"
+  },
+  {
+    "revision": "815210066c88799278d8",
+    "url": "/static/js/2.6f010cd3.chunk.js"
+  },
+  {
+    "revision": "0feae1450065efdcfdf6",
+    "url": "/static/js/main.d2dd9794.chunk.js"
+  },
+  {
+    "revision": "434872893da42ccf6f6f",
+    "url": "/static/js/runtime-main.9f9cf6c2.js"
+  }
+]);
