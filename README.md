23534f3d6ed9f3d46d40fc9e61c33e0b7796c4b5
Barnicle <egalimov95@gmail.com>
2020-02-09 21:45:15 +0300

    latest build

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 My first React project
 Simple todo list 
-
-  ![localhost_3000_](C:\Users\egali\Downloads\localhost_3000_.png)
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.773ab6c4db69b53ef263007fadaf4d41.js"
+  "./precache-manifest.adeced30ecb88f7a46dfe522f3ccecef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
